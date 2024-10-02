bbf5ae015aa348ca88ebc79205c37d09867614f6
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-10-02 20:37:15 +0530

    created order admin and cart pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// Importing components
+import Add from './components/Admin/Add/Add';
+import List from './components/Admin/List/List';
+import Navbar from './components/Admin/Navbar/Navbar';
+import Orders from './components/Admin/Orders/Orders';
+import Sidebar from './components/Admin/Sidebar/Sidebar';
+import Cart from './components/Cart/Cart';
+import PlaceOrder from './components/Placeorder/PlaceOrder';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/admin/add" element={<Add />} />
+          <Route path="/admin/list" element={<List />} />
+          <Route path="/admin/navbar" element={<Navbar />} />
+          <Route path="/admin/orders" element={<Orders />} />
+          <Route path="/admin/sidebar" element={<Sidebar />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
