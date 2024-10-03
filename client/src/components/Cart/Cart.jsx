470c452a3b4ff9a31c869d99fafdccb534ea8372
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-10-03 09:03:05 +0530

    created login page

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import "./Cart.css";
-import { StoreContext } from "../../context/StoreContext";
+import { StoreContext } from "../../StoreContext.jsx";
 
 function Cart() {
     const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
