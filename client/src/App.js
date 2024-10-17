47629005d9351eb3d42719f928c866f3765f88bd
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-10-17 19:34:34 +0530

    created home page and menu list

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,20 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
-// Importing components
-import Add from './components/Admin/Add/Add';
-import List from './components/Admin/List/List';
-import Navbar from './components/Admin/Navbar/Navbar';
-import Orders from './components/Admin/Orders/Orders';
-import Sidebar from './components/Admin/Sidebar/Sidebar';
-import Cart from './components/Cart/Cart';
-import PlaceOrder from './components/Placeorder/PlaceOrder';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/admin/add" element={<Add />} />
-          <Route path="/admin/list" element={<List />} />
-          <Route path="/admin/navbar" element={<Navbar />} />
-          <Route path="/admin/orders" element={<Orders />} />
-          <Route path="/admin/sidebar" element={<Sidebar />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/" element={<PlaceOrder />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/cart" element={<Cart/>}/>
+        <Route path = "/order" element={<PlaceOrder/>}/>
+      </Routes>
+    </div>
   );
 }
 
