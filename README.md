5a09c777e5cd2b660a688cc28eacaaedf186ae6d
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-10-19 20:24:34 +0530

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Zwigato_LOP" 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const[menu,setMenu] = useState("home");
     return (
         <div className='navbar'>
-            <img src={assets.logo} alt="" className='logo'/>
+            <img src={assets.logo} className='logo'/>
             <ul className="navbar-menu">
                 <li onClick={() => {setMenu("home")}} className={menu==="home" ? "active":""}>Home</li>
                 <li onClick={() => {setMenu("menu")}} className={menu==="menu" ? "active":""}>Menu</li>
