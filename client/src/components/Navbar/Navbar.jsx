3cb16de6a48fce4da1ec79c63179a85ee046510a
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-12-09 07:22:06 +0530

    solving bugs

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../Context/StoreContext";
 import "./Navbar.css";
 
-const Navbar = ({ setLogin, scrollToSection }) => {
+const Navbar = ({ setLogin }) => {
+    const nav = useNavigate();
     const [menu, setMenu] = useState("home");
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [showDropdown, setShowDropdown] = useState(false);
@@ -17,7 +19,6 @@ const Navbar = ({ setLogin, scrollToSection }) => {
     const handleMenuClick = (menuName) => {
         setMenu(menuName);
         setIsMenuOpen(false);
-        scrollToSection(menuName);
     };
 
     const handleLogout = () => {
@@ -26,33 +27,71 @@ const Navbar = ({ setLogin, scrollToSection }) => {
         setLogin(false);
     };
 
-    const handleNav = () => {};
+    const handleNav = () => {
+        nav('/myorders');
+    }
 
     return (
         <div className="navbar">
-            <img src={assets.logo} className="logo" alt="Logo" />
+            <Link to="/">
+                <img src={assets.logo} className="logo" alt="Logo" />
+            </Link>
             <ul className={`navbar-menu ${isMenuOpen ? "active" : ""}`}>
-                <li onClick={() => handleMenuClick("home")} className={menu === "home" ? "active" : ""}>Home</li>
-                <li onClick={() => handleMenuClick("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
-                <li onClick={() => handleMenuClick("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile App</li>
-                <li onClick={() => handleMenuClick("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact Us</li>
+                <li
+                    onClick={() => handleMenuClick("home")}
+                    className={menu === "home" ? "active" : ""}
+                >
+                    Home
+                </li>
+                <li
+                    onClick={() => handleMenuClick("menu")}
+                    className={menu === "menu" ? "active" : ""}
+                >
+                    Menu
+                </li>
+                <li
+                    onClick={() => handleMenuClick("mobile-app")}
+                    className={menu === "mobile-app" ? "active" : ""}
+                >
+                    Mobile App
+                </li>
+                <li
+                    onClick={() => handleMenuClick("contact-us")}
+                    className={menu === "contact-us" ? "active" : ""}
+                >
+                    Contact Us
+                </li>
             </ul>
             <div className="navbar-right">
                 <div className="navbar-search-icon">
-                    <img src={assets.basket_icon} alt="Basket" />
+                    <Link to="/cart">
+                        <img src={assets.basket_icon} alt="Basket" />
+                    </Link>
                 </div>
                 {token ? (
                     <div className="navbar-profile">
-                        <img src={assets.profile_icon} alt="Profile" onClick={toggleDropdown} />
+                        <img
+                            src={assets.profile_icon}
+                            alt="Profile"
+                            onClick={toggleDropdown}
+                        />
                         {showDropdown && (
                             <div className="profile-dropdown">
-                                <button className="dropdown-btn" onClick={handleNav}>Orders</button>
-                                <button className="dropdown-btn" onClick={handleLogout}>Logout</button>
+                                <button className="dropdown-btn" onClick={handleNav}>
+                                    <img src={assets.bag_icon} alt="Orders" />
+                                    Orders
+                                </button>
+                                <button className="dropdown-btn" onClick={handleLogout}>
+                                    <img src={assets.logout_icon} alt="Logout" />
+                                    Logout
+                                </button>
                             </div>
                         )}
                     </div>
                 ) : (
-                    <button className="sign-in-btn" onClick={() => setLogin(true)}>Sign-In</button>
+                    <button className="sign-in-btn" onClick={() => setLogin(true)}>
+                        Sign-In
+                    </button>
                 )}
             </div>
             <div className="hamburger" onClick={toggleMenu}>
