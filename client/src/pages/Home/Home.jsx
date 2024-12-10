24ab34d50900cf2fe03988cdefe82b6f1d38448d
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-12-10 18:36:49 +0530

    solving bugs

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -8,22 +8,6 @@ import "./Home.css";
 const Home = () => {
     const [category, setCategory] = useState("All");
 
-    const homeRef = useRef(null);
-    const menuRef = useRef(null);
-    const mobileAppRef = useRef(null);
-    const contactUsRef = useRef(null);
-
-    const scrollToSection = (section) => {
-        if (section === "home" && homeRef.current) {
-            homeRef.current.scrollIntoView({ behavior: "smooth" });
-        } else if (section === "menu" && menuRef.current) {
-            menuRef.current.scrollIntoView({ behavior: "smooth" });
-        } else if (section === "mobile-app" && mobileAppRef.current) {
-            mobileAppRef.current.scrollIntoView({ behavior: "smooth" });
-        } else if (section === "contact-us" && contactUsRef.current) {
-            contactUsRef.current.scrollIntoView({ behavior: "smooth" });
-        }
-    };
 
     return (
         <div>
